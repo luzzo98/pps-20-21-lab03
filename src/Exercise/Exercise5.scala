6de14fd9280ae91be4a11e8b3aa7da6f6a97302c
Simone Luzi <simone.luzi@studio.unibo.it>
2021-03-17 00:17:53 +0100

    exercise 5 and related test done

---
+++ src/Exercise/Exercise5.scala
@@ -0,0 +1,57 @@
+package Exercise
+
+import u03.Lists._
+import scala.annotation.tailrec
+
+object Exercise5 {
+
+  sealed trait Stream[A]
+
+  object Stream {
+
+
+    //Exercise 5
+    @tailrec
+    def drop[A](stream: Stream[A])(n: Int): Stream[A] = (stream,n) match {
+      case (Cons(_, tail), n) if n>1 => drop(tail())(n-1)
+      case (Cons(_, tail), n) if n==1 => tail()
+      case _ => stream
+    }
+
+
+
+    private case class Empty[A]() extends Stream[A]
+    private case class Cons[A](head: () => A, tail: () => Stream[A]) extends Stream[A]
+
+    def empty[A](): Stream[A] = Empty()
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+
+    def toList[A](stream: Stream[A]): List[A] = stream match {
+      case Cons(h,t) => List.Cons(h(), toList(t()))
+      case _ => List.Nil()
+    }
+
+    def map[A, B](stream: Stream[A])(f: A => B): Stream[B] = stream match {
+      case Cons(head, tail) => cons(f(head()), map(tail())(f))
+      case _ => Empty()
+    }
+
+    def filter[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match {
+      case Cons(head, tail) if pred(head()) => cons(head(), filter(tail())(pred))
+      case Cons(_, tail) => filter(tail())(pred)
+      case _ => Empty()
+    }
+
+    def take[A](stream: Stream[A])(n: Int): Stream[A] = (stream,n) match {
+      case (Cons(head, tail), n) if n>0 => cons(head(), take(tail())(n - 1))
+      case _ => Empty()
+    }
+
+    def iterate[A](init: => A)(next: A => A): Stream[A] = cons(init, iterate(next(init))(next))
+  }
+}
---
+++ test/TestExercise/TestExercise5.scala
@@ -0,0 +1,20 @@
+package TestExercise
+
+import u03.Lists.List._
+import Exercise.Exercise5._
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class TestExercise5 {
+
+  val s = Stream.take(Stream.iterate(0)(_+1))(10)
+  val short = Stream.take(Stream.iterate(0)(_+1))(2)
+
+
+  @Test def testDrop(): Unit = {
+    assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
+    assertEquals(Stream.toList(s), Stream.toList(Stream.drop(s)(0))) //with 0 returned the entire stream
+    assertEquals(Stream.empty(), Stream.drop(short)(5)) //if the stream is shorter than n an Empty() is returned
+    assertEquals(Nil(), Stream.toList(Stream.drop(Stream.empty())(5))) //if the stream is an Empty(), another Empty() is returned
+  }
+}
