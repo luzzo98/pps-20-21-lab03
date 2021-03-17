888b4ed9ee8cc9249c0765ea0c4e4463740a7251
Simone Luzi <simone.luzi@studio.unibo.it>
2021-03-17 11:35:05 +0100

    refactored and commented exercise1

--- src/Exercise/Exercise2.scala
+++ src/Exercise/Exercise2.scala
@@ -10,14 +10,15 @@ import scala.annotation.tailrec
 object Exercise2 {
 
   def max(l: List[Int]): Option[Int] = {
-    @tailrec def _max(l: List[Int], n: Int): Option[Int] = l match {
+    @tailrec
+    def _max(l: List[Int], n: Int): Option[Int] = l match {
       case Cons(h, t) => if (h > n) _max(t, h) else _max(t, n)
       case Nil() => Some(n)
     }
 
     l match {
       case Cons(h, t) => _max(t, h)
-      case Nil() => None()
+      case Nil() => None() //without this case "max(Nil())" would return an error
     }
   }
 }
--- test/TestExercise/TestExercise2.scala
+++ test/TestExercise/TestExercise2.scala
@@ -10,7 +10,7 @@ class TestExercise2 {
 
   @Test def testMax(): Unit = {
     assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
-    assertEquals(Some(20), max(Cons(-5, Cons(0, Cons(20, Nil())))))
-    assertEquals(None(), max(Nil()))
+    assertEquals(Some(20), max(Cons(-5, Cons(0, Cons(20, Cons(20, Cons(10, Nil())))))))
+    assertEquals(None(), max(Nil())) //test with nil()
   }
 }
